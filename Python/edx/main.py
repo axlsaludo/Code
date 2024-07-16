a1324494f2393c6c541e6a6748a04b561160d553
Kurt Axl Saludo <79072016+axlsaludo@users.noreply.github.com>
2024-07-16 23:16:42 +0800

    up

--- Python/edx/main.py
+++ Python/edx/main.py
@@ -1,3 +1,12 @@
-from gpiozero import LED
+import cv2
 
-print (adc.value)
+videoCapture = cv2.VideoCapture(0)
+fps = 30
+size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+while True:
+    ret, frame = videoCapture.read()
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
